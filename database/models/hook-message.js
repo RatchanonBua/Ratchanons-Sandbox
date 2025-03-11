2952eb08cb3e01c19594b0512ce2a9a71cd5c7dc
Ratchanon Bualeesornsakun <ratchanon.blssk@gmail.com>
2025-03-11 20:52:25 +0700

    เพิ่ม-แก้ไข Schema ของระบบ Webhook / ลบ shard-config.js ออกชั่วคราว

--- database/models/hook-message.js
+++ database/models/hook-message.js
@@ -1,5 +1,28 @@
 const mongoose = require('mongoose');
 
+const messageContextSchema = new mongoose.Schema({
+  keywords: {
+    type: [String],
+    default: [],
+  },
+  quoteToken: {
+    type: String,
+    default: null,
+  },
+  isRedelivery: {
+    type: Boolean,
+    default: false,
+  },
+  timestampSent: {
+    type: Date,
+    default: null,
+  },
+  quotedMessageId: {
+    type: String,
+    default: null,
+  },
+}, { _id: false });
+
 const hookMessageSchema = new mongoose.Schema({
   hookUser: {
     type: mongoose.Schema.Types.ObjectId,
@@ -9,26 +32,45 @@ const hookMessageSchema = new mongoose.Schema({
   hookOrigin: {
     type: String,
     enum: ['line', 'facebook', 'google'],
-    default: 'line',
+    required: true,
+  },
+  hookEventId: {
+    type: String,
+    default: null,
+  },
+  messageId: {
+    type: String,
+    required: true,
+  },
+  quotedMessage: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'HookMessage',
+    default: null,
   },
   messageType: {
     type: String,
-    enum: ['text', 'image', 'video', 'audio', 'file', 'sticker'],
+    enum: ['text', 'image', 'video', 'audio', 'file', 'location', 'sticker'],
     required: true,
     default: 'text',
   },
   messageText: {
     type: String,
     default: null,
-    required: function () {
-      return this.messageType === 'text';
+    validate: {
+      validator: function (v) {
+        return (this.messageType === 'text' || this.messageType === 'location') ? v !== null : true;
+      },
+      message: "messageText is required for text and location type",
     },
   },
   mediaUrl: {
     type: String,
     default: null,
-    required: function () {
-      return ['image', 'video', 'audio', 'file', 'sticker'].includes(this.messageType);
+    validate: {
+      validator: function (v) {
+        return (['image', 'video', 'audio', 'file', 'sticker'].includes(this.messageType)) ? v !== null : true;
+      },
+      message: "mediaUrl is required for image, video, audio, file, or sticker type",
     },
   },
   messageStatus: {
@@ -40,6 +82,10 @@ const hookMessageSchema = new mongoose.Schema({
     type: String,
     enum: ['in', 'out'],
     default: 'in',
+  },
+  messageContext: {
+    type: messageContextSchema,
+    default: {},
   },
   adminUserId: {
     // Admin User ID
@@ -63,6 +109,11 @@ const hookMessageSchema = new mongoose.Schema({
 }, {
   timestamps: true,
 });
+
+hookMessageSchema.index({ hookUser: 1 });
+hookMessageSchema.index({ hookOrigin: 1, hookEventId: 1 }, { unique: true });
+hookMessageSchema.index({ hookOrigin: 1, messageId: 1 }, { unique: true });
+hookMessageSchema.index({ quoteMessage: 1 });
 
 const HookMessage = mongoose.model('HookMessage', hookMessageSchema, 'hook_messages');
 module.exports = HookMessage;
