c213579c6bb86c20cb240015b5b751235fd93681
Ratchanon Bualeesornsakun <ratchanon.blssk@gmail.com>
2025-03-13 20:35:03 +0700

    เพิ่มการบันทึกข้อมูลแชทจาก Webhook ประเภท Text และ Sticker

--- admin/backend/src/modules/webhooks/controllers/line.controller.js
+++ admin/backend/src/modules/webhooks/controllers/line.controller.js
@@ -1,6 +1,6 @@
 const { getLineProfile } = require('@/utils/line-api');
 
-// const hookMessageModel = require('@/modules/webhooks/models/hook-message.model');
+const hookMessageModel = require('@/modules/webhooks/models/hook-message.model');
 const hookUserModel = require('@/modules/webhooks/models/hook-user.model');
 
 const processLineWebhook = async (payload) => {
@@ -9,11 +9,12 @@ const processLineWebhook = async (payload) => {
     const eventList = payload.events;
     for (const eventData of eventList) {
       if (eventData.source?.userId) {
+        // ---- ---- ---- ---- //
         // Initial Variables
         const externalUserId = eventData.source.userId;
         const replyToken = eventData.replyToken;
         const timestamp = eventData.timestamp;
-        let hookUserId = "";
+        let hookUserId = '';
         // Search User for Create or Update
         const targetUser = {
           hookOrigin: 'line',
@@ -26,13 +27,13 @@ const processLineWebhook = async (payload) => {
         const infoUser = {
           externalReplyToken: replyToken,
           displayName: displayName,
-          lastActivityAt: (timestamp) ? new Date(timestamp) : new Date(),
+          // lastActivityAt: (timestamp) ? new Date(timestamp) : new Date(),
         };
         // LINE Profile
         const profileData = await getLineProfile(externalUserId);
         if (profileData) {
           infoUser.profileImage = profileData.pictureUrl;
-          infoUser.displayName = profileData.displayName;
+          infoUser.displayName = profileData.displayName || displayName;
         }
         // Check Search Result
         if (hookUserObj) {
@@ -45,6 +46,88 @@ const processLineWebhook = async (payload) => {
           hookUserObj = await hookUserModel.createHookUser(createUser);
           hookUserId = hookUserObj._id;
         }
+        // ---- ---- ---- ---- //
+        // Message Type
+        if (eventData.type === 'message') {
+          // Initial Variables
+          const hookMessage = eventData.message;
+          let hookMessageId = '';
+          // Check Message Object
+          if (hookMessage && typeof hookMessage === 'object' && !Array.isArray(hookMessage)) {
+            // Search Message for Create or Update
+            const targetMessage = {
+              hookUser: hookUserId,
+              hookOrigin: 'line',
+              hookEventId: eventData.webhookEventId,
+            };
+            let hookMessageObj = await hookMessageModel.fetchHookMessage('one', targetMessage, {}, '_id');
+            // Query Quoted Message
+            let quotedMessageId = null;
+            if (hookMessage.quotedMessageId) {
+              const targetQuotedMessage = {
+                hookOrigin: 'line',
+                messageId: hookMessage.quotedMessageId,
+              };
+              let quotedMessageObj = await hookMessageModel.fetchHookMessage('one', targetQuotedMessage, {}, '_id');
+              if (quotedMessageObj) { quotedMessageId = quotedMessageObj._id; }
+            }
+            // Build Data for Create or Update
+            const infoMessage = {
+              messageId: hookMessage.id,
+              quotedMessage: quotedMessageId,
+              messageStatus: 'success',
+              messageDirection: 'in',
+              messageContext: {
+                quoteToken: hookMessage.quoteToken || null,
+                quotedMessageId: hookMessage.quotedMessageId || null,
+                isRedelivery: eventData.deliveryContext?.isRedelivery || false,
+                timestampSent: (timestamp) ? new Date(timestamp) : new Date(),
+              },
+            };
+            // Check Message Type
+            let shortMessageText = '';
+            let isValidMessageType = false;
+            // ---- ---- ---- ---- //
+            if (hookMessage.type === 'text') {
+              if (hookMessage.emojis) { continue; }
+              const messageText = hookMessage.text;
+              // Set Info Message
+              infoMessage.messageType = 'text';
+              infoMessage.messageText = messageText;
+              // Valid Message
+              shortMessageText = messageText.substring(0, 252) + "...";
+              isValidMessageType = true;
+            } else if (hookMessage.type === 'sticker' && hookMessage.stickerId) {
+              // Set Info Message
+              infoMessage.messageType = 'sticker';
+              infoMessage.mediaUrl = `https://stickershop.line-scdn.net/stickershop/v1/sticker/${hookMessage.stickerId}/android/sticker.png`;
+              infoMessage.messageContext.stickerId = hookMessage.stickerId || null;
+              infoMessage.messageContext.packageId = hookMessage.packageId || null;
+              infoMessage.messageContext.stickerType = hookMessage.stickerResourceType || null;
+              infoMessage.messageContext.keywords = hookMessage.keywords || [];
+              // Valid Message
+              shortMessageText = 'Sticker Message'
+              isValidMessageType = true;
+            }
+            // ---- ---- ---- ---- //
+            // Check Search Result
+            if (isValidMessageType) {
+              const lastActivityAt = (timestamp) ? new Date(timestamp) : new Date();
+              if (hookMessageObj) {
+                // Update Data
+                hookMessageId = hookMessageObj._id;
+                await hookMessageModel.updateHookMessage('id', { _id: hookMessageId }, infoMessage, true);
+              } else {
+                // Insert Data
+                const createMessage = Object.assign({}, targetMessage, infoMessage);
+                hookMessageObj = await hookMessageModel.createHookMessage(createMessage);
+                hookMessageId = hookMessageObj._id;
+              }
+              await hookUserModel.updateHookUser('id', { _id: hookUserId }, { lastMessage: shortMessageText, lastActivityAt: lastActivityAt }, true);
+            }
+          }
+        }
+        // ---- ---- ---- ---- //
       }
     }
   }
--- admin/backend/src/modules/webhooks/models/hook-message.model.js
+++ admin/backend/src/modules/webhooks/models/hook-message.model.js
@@ -4,24 +4,60 @@ const createHookMessage = async (objectData) => {
   try {
     const hookMessage = await HookMessage.create(objectData);
     return hookMessage;
-  } catch {
+  } catch (error) {
     console.error('Error Creating Hook Message:', error);
     throw error;
   }
 };
 
-const fetchHookMessage = async () => {
+// mode = ['all', 'one', 'id']
+const fetchHookMessage = async (mode = 'all', condition = {}, sort = {}, field = '') => {
   try {
-    return undefined;
+    let query = HookMessage.find(condition);
+    // Search Mode
+    if (mode === 'one') {
+      query = HookMessage.findOne(condition);
+    } else if (mode === 'id' && condition?._id) {
+      query = HookMessage.findById(condition._id);
+    }
+    // Sort Result
+    if (Object.keys(sort).length) {
+      query = query.sort(sort);
+    }
+    // Fields to Select
+    if (typeof field === 'string' && field !== '') {
+      query = query.select(field);
+    }
+    // Search Operation
+    const result = await query;
+    return (mode === 'one' || (mode === 'id' && condition?._id)) ? result : result || [];
   } catch (error) {
     console.error('Error Fetching Hook Message:', error);
     throw error;
   }
 };
 
-const updateHookMessage = async () => {
+// mode = ['id', 'one', 'many']
+const updateHookMessage = async (mode = 'id', condition = {}, data = {}, timestamp = true) => {
   try {
-    return undefined;
+    // Check Condition
+    if (Object.keys(condition).length === 0) {
+      return null;
+    }
+    // Update Mode
+    let query = HookMessage.updateMany(condition, data);
+    if (mode === 'id' && condition?._id) {
+      query = HookMessage.findByIdAndUpdate(condition._id, data, { new: true });
+    } else if (mode === 'one') {
+      query = HookMessage.findOneAndUpdate(condition, data, { new: true });
+    }
+    // Update Timestamps
+    if (!timestamp) {
+      query = query.setOptions({ timestamps: false });
+    }
+    // Update Operation
+    const result = await query;
+    return result;
   } catch (error) {
     console.error('Error Updating Hook Message:', error);
     throw error;
